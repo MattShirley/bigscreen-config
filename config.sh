258fc9119f3b17876d1ac89997064ad22c840ec6
MattShirley <mattutshirley@gmail.com>
2021-07-09 20:03:57 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,6 +4,7 @@ sudo apt upgrade
 sudo apt install -y build-essential git perl python cmake gettext vim
 sudo apt install -y extra-cmake-modules plasma-workspace-dev libkf5activities-dev libkf5activitiesstats-dev libkf5i18n-dev kirigami2-dev libkf5declarative-dev libkf5kcmutils-dev libkf5notifications-dev libkf5kio-dev libkf5wayland-dev
 
+# revisit whether the following are necessary
 sudo apt-get install qttools5-dev-tools libqt5svg5-dev qtmultimedia5-dev
 
 git clone https://invent.kde.org/plasma/plasma-nano.git
